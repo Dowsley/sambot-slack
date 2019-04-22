3cf7ccc8b23efe002f57382dc688c6c25b5c7321
João Felipe <joao.dowsley850@gmail.com>
2019-04-22 15:01:02 -0300

    Update sambot_id_fetch.py

--- sambot_id_fetch.py
+++ sambot_id_fetch.py
@@ -3,12 +3,10 @@ from slackclient import SlackClient
 sc = SlackClient('xoxb-601279383251-616213772279-sffjqQxH5C6eh2zL26dhsUOI')
 
 
-
-def fetch_id():
-	email_input = input("Email: ")
+def fetch_id(email):
 	user_info=sc.api_call(
 			"users.lookupByEmail",
-			email=email_input,
+			email=email,
 			)
 	return user_info['user']
 
@@ -16,14 +14,16 @@ def fetch_id():
 def send_message(userid):
     sc.api_call(
         "chat.postMessage",
+        as_user=True,
         channel=userid,
-        text="Seu report foi enviado com sucesso. Para receber mais feedback, utilize os seguintes comandos:\n/status para ver o estado de resolução do seu problema. ",
-        username='SAMBOT',
-        icon_emoji=':dog:'
+        text="Seu report foi enviado com sucesso. Para receber mais feedback, utilize os seguintes comandos:\n/statusrelatorio para ver o estado de resolução do seu problema. ",
     )
 
-if __name__ == '__main__':
- 	u=fetch_id()
- 	send_message(u['id'])
- 	print("Success!")
 
+if __name__ == '__main__':
+    email_input = input("Email: ")
+    u=fetch_id(email_input)
+    send_message(u['id'])
+    print("Successo: Primeira mensagem enviada!")
+
+    
