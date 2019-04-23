4e086ff71f55a26e1d21116cfdfbb88ee805b71f
João Felipe <joao.dowsley850@gmail.com>
2019-04-23 10:05:23 -0300

    Update sambot_id_fetch.py

--- sambot_id_fetch.py
+++ sambot_id_fetch.py
@@ -16,14 +16,45 @@ def send_message(userid):
         "chat.postMessage",
         as_user=True,
         channel=userid,
-        text="Seu report foi enviado com sucesso. Para receber mais feedback, utilize os seguintes comandos:\n/statusrelatorio para ver o estado de resolução do seu problema. ",
+        text="Seu report foi enviado com sucesso. Para receber mais feedback, utilize os seguintes comandos:\n--status para ver o estado de resolução do seu problema.\n --cancelar para cancelar seu último report.",
     )
 
+
+def commands(userid, status):
+    conv_info=sc.api_call(
+        "conversations.open",
+        users=userid,
+        return_im=True,
+    )
+    ult_msg=conv_info['channel']['latest']['text']
+
+    if ult_msg=='--status':
+        sc.api_call(
+            "chat.postMessage",
+            as_user=True,
+            channel=userid,
+            text=status,
+            )
+        print("Comando --status detectado.")
+
+    elif ult_msg=='--cancelar':
+        sc.api_call(
+            "chat.postMessage",
+            as_user=True,
+            channel=userid,
+            text="Report cancelado com sucesso",
+            )
+        return "O report foi cancelado por você."
+        print("Comando --cancelar detectado")
+    else:
+        return status
 
 if __name__ == '__main__':
     email_input = input("Email: ")
     u=fetch_id(email_input)
     send_message(u['id'])
     print("Successo: Primeira mensagem enviada!")
+    status="A equipe já foi mobilizada. Você será notificado quando seu problema for resolvido"
+    while 'true'=='true':
+        status=commands(u['id'], status)
 
-    
