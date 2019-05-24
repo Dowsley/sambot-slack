ca2d71a786adf50ae1ee55a145f834ab3bd3735e
unknown <joao.dowsley850@gmail.com>
2019-05-24 11:11:56 -0300

    Organized Stuff 1!

--- database_connector.py
+++ database_connector.py
@@ -1,6 +1,7 @@
 import MySQLdb # Lib para banco de dados MySQL
 import time # Funções de delay
 
+
 # Declara acesso ao banco de dados
 con = MySQLdb.connect(
 	host="localhost",
@@ -11,8 +12,10 @@ con = MySQLdb.connect(
 	)
 print(con)
 
+
 # Parametros do cursor estão definidos para retornar um dict
 c = con.cursor(MySQLdb.cursors.DictCursor) 
+
 
 # Função que devolve dados determinados de uma tabela
 def select(fields, tables, where=None):
@@ -25,8 +28,10 @@ def select(fields, tables, where=None):
 	c.execute(query)
 	return c.fetchall()
 
+
 # Determina quantidade inicial de reports, irá servir de comparador para saber se existe report um novo
 aux = len(select("id_usuarios_report","usuarios_report"))
+
 
 # Loop que realiza a detecção de um report novo e, caso aconteça, quebra o loop e entra no BOT.
 print("Detecção iniciada...")
--- sambotv2.py
+++ sambotv2.py
@@ -1,41 +1,43 @@
+# Olá! Este SCRIPT (sambotv2) é o componente principal do programa, responsável por executar o BOT enquanto está rodando.
+# Ele foi dividido em algumas partes para facilitar seu entendimento e execução.
+# Para o BOT funcionar, o SCRIPT precisa resgatar informações de um novo report no banco de dados MySQL...
+# ... e é isso que o segundo script (database_connector) faz: Mantém uma constante detecção e só para quando um novo report entra.
+# Assim que as informações do novo report são importadas, a execução é retomada e o BOT começa a funcionar.
+
+
+
+# --------------------------------------- IMPORT E CONEXÃO ---------------------------------------
 import MySQLdb # Lib para conectar com o banco de dados MySQL
 import time  # Necessária para fazer delays no loop
 from slackclient import SlackClient  # Lib principal para o bot funcionar
-from database_connector import db_userinfo # Variável que carrega os dados de usuario do novo request
-sc = SlackClient('xoxb-601279383251-616213772279-sffjqQxH5C6eh2zL26dhsUOI') # Token de controle do BOT
+from database_connector import db_userinfo # Variável que carrega os dados de usuario do novo request, executa segundo Script.
 
-# Informações individuais sobre o report
-email_input = db_userinfo['email_usuario']
-problema_input = db_userinfo['problema_reportado']
-status_input = db_userinfo['status']
-id_usuarios_report = db_userinfo['id_usuarios_report']
+sc = SlackClient('xoxb-601279383251-616213772279-sffjqQxH5C6eh2zL26dhsUOI') # Conexão com o token de controle do BOT
 
-print("Email detectado:", email_input)
-print("Problema detectado:", problema_input)
-
-# Conexão com o banco de dados
-con = MySQLdb.connect(
+con = MySQLdb.connect( # Conexão com o banco de dados
     host="localhost",
     user="root",
     password="s4mb0t_cesarschool",
     port = 3306,
     db="database_report"
     )
+
 print(con)
 
-#Parametros do cursor estão definidos para retornar um dict
-c = con.cursor(MySQLdb.cursors.DictCursor) 
+c = con.cursor(MySQLdb.cursors.DictCursor) # Parametros do cursor estão definidos para retornar um dict
 
-# Seletor de informações
-def select(fields, tables, where): 
+
+
+# --------------------------------------- FUNÇÕES ---------------------------------------
+def select(fields, tables, where): # Seleciona e devolve determinadas informações da DB.
     global c
     query = "SELECT " + fields + " FROM " + tables
     query += " WHERE " + where
     c.execute(query)
     return c.fetchall()
 
-# Função que realiza update de informações (atualmente só cancela report)
-def update(where):
+
+def update(where): # Função que realiza update (mudança) de informações na DB.
     global c, con
     query = "UPDATE usuarios_report"
     query += " SET status = 'Cancelado'"
@@ -43,23 +45,31 @@ def update(where):
     c.execute(query)
     con.commit()
 
-# Busca as info. de um usuário no workspace de acordo com o e-mail de entrada
-def fetch_info(email): 
+
+def fetch_info(email): # Busca as info. de um usuário no workspace de acordo com o e-mail de entrada
     user_info=sc.api_call(
             "users.lookupByEmail",
             email=email,
             )
     return user_info['user']
 
-# Manda a primeira mensagem do BOT para o usuário
-def send_firstmessage(slack_userinfo):
+
+def send_message(userid,text): # Manda uma mensagem (como BOT) para o usuário
+    sc.api_call(
+            "chat.postMessage",
+            as_user=True,
+            channel=userid,
+            text=text
+            )
+
+
+def send_firstmessage(slack_userinfo): # Manda a primeira mensagem do BOT para o usuário
     if slack_userinfo['profile']['display_name']=='':
         name=slack_userinfo['profile']['real_name']
     else:
         name=slack_userinfo['profile']['display_name']
 
     texto = "Olá " + name + "! Seu report foi enviado com sucesso. Para receber mais feedback, utilize os seguintes comandos:\n\nSTATUS para ver o estado de resolução do seu problema.\n\nCANCELAR para anular o seu report."
-
     sc.api_call(
         "chat.postMessage",
         as_user=True,
@@ -67,12 +77,12 @@ def send_firstmessage(slack_userinfo):
         text=texto,
     )
 
-# Reconhece comandos e os responde correspondentemente
-def commands(userid, status, problema_input): 
+
+def commands(userid, status, problema, primarykey): # Reconhece comandos e os responde correspondentemente
     conv_info=sc.api_call(
         "conversations.open",
         users=userid,
-        return_im=True,
+        return_im=True
     )
     
     ult_msg=conv_info['channel']['latest']['text']
@@ -80,59 +90,69 @@ def commands(userid, status, problema_input):
 
     if ult_msg == 'STATUS':
         if "não resolvido" in status.strip().lower() or "nao resolvido" in status.strip().lower():
-            text_status = problema_input + "Não resolvido. A equipe já foi mobilizada e você será avisado aqui quando o problema for solucionado"
+            text_status = problema + "Não resolvido. A equipe já foi mobilizada e você será avisado aqui quando o problema for solucionado."
         else:
-            text_status= problema_input + status
-        sc.api_call(
-            "chat.postMessage",
-            as_user=True,
-            channel=userid,
-            text=text_status,
-            )
-        print("Comando STATUS detectado.")
+            text_status= problema + status
+
+        send_message(userid, text_status)
+        print("Comando STATUS detectado.") # Debugger
 
     elif ult_msg=='CANCELAR':
+        global cancelar
         cancelar=True
-        sc.api_call(
-            "chat.postMessage",
-            as_user=True,
-            channel=userid,
-            text="Você tem certeza que quer cancelar seu report? Digite SIM para confirmar.",
-            )
-        print("Comando CANCELAR detectado")
-        update(id_usuarios_report)
+        text_status = "Você tem certeza que quer cancelar seu report? Digite SIM para confirmar ou NÃO para anular esta ação."
+        send_message(userid, text_status)
+        print("Comando CANCELAR detectado") # Debugger
 
-    elif ult_msg=='SIM' and confirm==True:
-        sc.api_call(
-            "chat.postMessage",
-            as_user=True,
-            channel=userid,
-            text="Report cancelado com sucesso",
-            )
-        print("CONFIRMAÇÃO de CANCELAR detectado")
-        update(id_usuarios_report)
+    elif ult_msg=='SIM' and cancelar==True:
+        text_status = "Report cancelado com sucesso"
+        send_message(userid, text_status)
+        update(primarykey)
+        print("CONFIRMAÇÃO de CANCELALAMENTO detectado") # Debugger
 
-# Estrutura de execução principal, chama todas as funções
+    elif (ult_msg=='NÃO' or ult_msg=='NAO') and cancelar==True:
+        cancelar = False
+        text_status = "Tudo bem, continuaremos o processo de resolução."
+        send_message(userid, text_status)
+        print("ANULAÇÃO de CANCELAMENTO detectado") # Debugger
+
+
+
+# --------------------------------------- RESGATE E DEFINIÇÃO DE INFORMAÇÕES ---------------------------------------
+# As informações do request (importadas no começo do script) são divididas em informações menores e definidas.
+
+email_input = db_userinfo['email_usuario'] # Email do usuário do Report
+problema_input = db_userinfo['problema_reportado'] # Problema reportado
+status_input = db_userinfo['status'] # Estado de resolução do problema
+primarykey_input = db_userinfo['id_usuarios_report'] #primarykey identificadora do report na tabela
+
+print("Email detectado:", email_input) # Debugger
+print("Problema detectado:", problema_input) # Debugger
+
+
+
+# --------------------------------------- EXECUÇÃO ---------------------------------------
+# Realiza todas as operações de bot num loop eterno (até o problema ser resolvido)
+
 if __name__ == '__main__':  
+    problema_input += ": "
     passagem=False
     cancelar=False
-    problema_input += ": "
+
     slack_userinfo=fetch_info(email_input)
     send_firstmessage(slack_userinfo)
     print("Successo: Primeira mensagem enviada!")
-    while 'sam'=='sam':
-        commands(slack_userinfo['id'], status_input, problema_input)
-        status_input=select("status", "usuarios_report", "id_usuarios_report={}".format(id_usuarios_report))[0]['status']        
+
+    while True:
+        commands(slack_userinfo['id'], status_input, problema_input,primarykey_input)
+        status_input=select("status", "usuarios_report", "id_usuarios_report={}".format(primarykey_input))[0]['status']        
         
         if status_input.lower() =="resolvido" and passagem==False:
-            sc.api_call(
-            "chat.postMessage",
-            as_user=True,
-            channel=slack_userinfo['id'],
-            text="Seu problema foi resolvido! Obrigado por contribuir com o SAM."
-            )
             passagem=True
-            print("Problema resolvido... Encerrando o script.")
+            text_status = "Seu problema foi resolvido! Obrigado por contribuir com o SAM. Fui!"
+            send_message(slack_userinfo['id'], text_status)
+            print("Problema resolvido... Encerrando o script.") # Debugger
+            break
 
         time.sleep(1)
         con.commit()
